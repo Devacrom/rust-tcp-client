5e752b677821619228d66ff5f0d8d87f54f02867
devacrom <devacrom@pop-os.localdomain>
2021-03-28 11:27:19 +0200

    separate threads input output

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 use std::io::prelude::*;
-use std::net::TcpStream;
+use std::net:: TcpStream;
+use std::thread;
+use std::process;
 
 pub mod messenger;
 
 fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:7878")
+    let ip ="127.0.0.1:3000";
+    let stream = TcpStream::connect(ip)
     .expect("Couldn't connect to the server...");
 
     fn connect(mut stream: &TcpStream)-> std::io::Result<()> {
-        let mut buffer = [0; 2048];
         let message = "greetings from rust client";
         stream.write(message.as_bytes())?;
-        stream.read(&mut buffer)?;
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        let mut rstream = stream.try_clone().unwrap();
+        thread::spawn(move ||{ 
+            let mut client_buffer = [0; 2048];           
+             loop {
+                 match rstream.read(&mut client_buffer) {
+                    Ok(n) => {
+                        if n == 0 {
+                            process::exit(0);
+                        }
+                        messenger::read_message(&rstream).expect("connection interrupted");
+                    }
+                    Err(error)=> println!("{}",error),
+                 }
+             }
+        });
         Ok(())
         
     }
@@ -21,11 +36,8 @@ fn main() {
     match connect(&stream) {
         Err(e) => println!("{:?}", e),
         _ => loop {
-            let mut buffer = [0; 2048];
-            messenger::send_message(&stream);
-            stream.read(&mut buffer).unwrap();
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-        }
+                messenger::send_message(&stream);
+           }
     }
         
 
--- src/messenger.rs
+++ src/messenger.rs
@@ -11,4 +11,13 @@ pub fn send_message(mut stream: &TcpStream) {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("error");
     stream.write(input.trim().as_bytes()).expect("ERROR: impossible to send message to ip, check the connection");
+    stream.flush().unwrap();
+}
+
+pub fn read_message(mut stream: &TcpStream)-> std::io::Result<()> {
+    let mut buffer = [0; 2048];
+    stream.read(&mut buffer).expect("connection interrupted");
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    Ok(())
+              
 }
